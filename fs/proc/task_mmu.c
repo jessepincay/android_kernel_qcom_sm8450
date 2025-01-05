672c286db3193dd5bbd452576a45f7e3af549e59
kanonifyX <kanonify01@gmail.com>
2025-01-05 09:06:48 +0700

    /proc/pid/maps: fix warning mixing declarations and code is a C99 extension [-Wdeclaration-after-statement]

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -348,27 +348,35 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	const char *name = NULL;
 
 	if (file) {
-		struct inode *inode = file_inode(vma->vm_file);
-		dev = inode->i_sb->s_dev;
-		ino = inode->i_ino;
-		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-        struct dentry *dentry = file->f_path.dentry;
-        if (dentry) {
-        	const char *path = (const char *)dentry->d_name.name; 
-            	if (strstr(path, "lineage")) { 
-	  	start = vma->vm_start;
-		end = vma->vm_end;
-		show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
-            	name = "/dev/ashmem (deleted)";
-		goto done;
-            	 	}
-            	if (strstr(path, "jit-zygote-cache")) { 
-	  	start = vma->vm_start;
-		end = vma->vm_end;
-		show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
-		goto bypass;
-            	 	}
+    struct inode *inode = file_inode(vma->vm_file);
+    struct dentry *dentry = NULL;
+    const char *path = NULL;
+
+    	if (inode) {
+        	dev = inode->i_sb->s_dev;
+        	ino = inode->i_ino;
+        	pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+
+        	dentry = file->f_path.dentry;
+        	if (dentry) {
+            	path = dentry->d_name.name;
+
+            	if (path && strstr(path, "lineage")) {
+                	start = vma->vm_start;
+                	end = vma->vm_end;
+                	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
+                	name = "/dev/ashmem (deleted)";
+                	goto done;
             	}
+
+            	if (path && strstr(path, "jit-zygote-cache")) {
+                	start = vma->vm_start;
+                	end = vma->vm_end;
+                	show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
+                	goto bypass;
+            	}
+        	}
+    	}
 	}
 
 	start = vma->vm_start;
