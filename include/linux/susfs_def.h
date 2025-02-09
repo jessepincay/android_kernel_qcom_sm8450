6611fcccfa383ebea6cb743b4adb22e4e0df358b
Joey <joey@evolution-x.org>
2025-02-09 06:00:19 +0100

    fs: Update SuSFS to v1.5.5

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -35,6 +35,10 @@
 #define SUS_SU_WITH_OVERLAY 1 /* deprecated */
 #define SUS_SU_WITH_HOOKS 2
 
+#define DEFAULT_SUS_MNT_ID 100000 /* used by mount->mnt_id */
+#define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
+#define DEFAULT_SUS_MNT_GROUP_ID 1000 /* used by mount->mnt_group_id */
+
 /*
  * inode->i_state => storing flag 'INODE_STATE_'
  * mount->mnt.susfs_orig_mnt_id => storing original mnt_id
